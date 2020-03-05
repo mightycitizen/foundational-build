12032d64a8935e9015e72ca3aa741ddb3562d136
johnle <john.le@mediacurrent.com>
2020-03-05 18:27:35 -0500

    MSCD-257: Work done so far for the quote carousel

---
+++ src/patterns/components/quote-carousel/quote-carousel.es6.js
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * Behaviors for the Filter Accordion.
+ */
+/* eslint-disable max-len */
+
+!((document, Drupal, $) => {
+  'use strict';
+
+  /**
+   * Setup and attach the Carousel behaviors.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.quoteCarousel = {
+    attach: function (context) {
+      $('.quote-carousel__slick', context).once('quoteCarousel')
+        .each(function() {
+          $(this).slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            fade: false,
+          });
+        });
+    }
+  };
+
+})(document, Drupal, jQuery);

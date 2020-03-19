c7fd6bca5aa7178299156a70f40be1d0b1e4eef5
johnle <john.le@mediacurrent.com>
2020-03-19 18:18:53 -0400

    MSCD-262: Component integration and styling changes for the event carousel component

--- dist/js/all.js
+++ dist/js/all.js
@@ -171,6 +171,37 @@
    * @type {Drupal~behavior}
    */
 
+  Drupal.behaviors.eventCarousel = {
+    attach: function attach(context) {
+      $('.event-carousel__slick', context).once('eventCarousel').each(function () {
+        var $nav = $(this).parent().find('.slick-pager');
+        $(this).slick({
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          arrows: true,
+          fade: false,
+          appendArrows: $nav
+        });
+      });
+    }
+  };
+}(document, Drupal, jQuery);
+//# sourceMappingURL=event-carousel.es6.js.map
+
+/**
+ * @file
+ * Behaviors for the Filter Accordion.
+ */
+
+/* eslint-disable max-len */
+!function (document, Drupal, $) {
+  'use strict';
+  /**
+   * Setup and attach the Carousel behaviors.
+   *
+   * @type {Drupal~behavior}
+   */
+
   Drupal.behaviors.galleryCarousel = {
     attach: function attach() {
       $('.gallery-carousel').not('.slick-initialized').slick({
---
+++ src/patterns/components/event-carousel/event-carousel.es6.js
@@ -0,0 +1,31 @@
+/**
+ * @file
+ * Behaviors for the Filter Accordion.
+ */
+/* eslint-disable max-len */
+
+!((document, Drupal, $) => {
+  'use strict';
+
+  /**
+   * Setup and attach the Carousel behaviors.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.eventCarousel = {
+    attach: function (context) {
+      $('.event-carousel__slick', context).once('eventCarousel')
+        .each(function () {
+          let $nav = $(this).parent().find('.slick-pager');
+          $(this).slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: true,
+            fade: false,
+            appendArrows: $nav,
+          });
+        });
+    }
+  };
+
+})(document, Drupal, jQuery);

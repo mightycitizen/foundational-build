3c38d4e0ad237d91b9065829d16dbff1bbcf4a63
Justin Rent <justin.rent@mediacurrent.com>
2020-04-20 15:26:13 -0400

    BEATCOVID-54 Refine behavior of nav items for responsiveness

--- dist/js/all.js
+++ dist/js/all.js
@@ -1322,13 +1322,18 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
         var $parent = $self.parent();
         var $target = $self.siblings('.menu__submenu');
 
-        if ($parent.hasClass('js-opened')) {
-          $parent.removeClass('js-opened');
-          $target.attr('aria-hidden', 'true');
-        } else {
-          $parent.addClass('js-opened');
-          $target.attr('aria-hidden', 'false');
+        if ($('.main-menu__navicon', context).is(':visible')) {
+          if ($parent.hasClass('js-opened')) {
+            $parent.removeClass('js-opened');
+            $target.attr('aria-hidden', 'true');
+          } else {
+            $parent.addClass('js-opened');
+            $target.attr('aria-hidden', 'false');
+          }
         }
+      });
+      $(window).resize(function () {
+        $('li.menu-item--has-submenu').removeClass('js-opened');
       });
     }
   };
--- dist/js/main-menu.js
+++ dist/js/main-menu.js
@@ -13,13 +13,18 @@
         var $parent = $self.parent();
         var $target = $self.siblings('.menu__submenu');
 
-        if ($parent.hasClass('js-opened')) {
-          $parent.removeClass('js-opened');
-          $target.attr('aria-hidden', 'true');
-        } else {
-          $parent.addClass('js-opened');
-          $target.attr('aria-hidden', 'false');
+        if ($('.main-menu__navicon', context).is(':visible')) {
+          if ($parent.hasClass('js-opened')) {
+            $parent.removeClass('js-opened');
+            $target.attr('aria-hidden', 'true');
+          } else {
+            $parent.addClass('js-opened');
+            $target.attr('aria-hidden', 'false');
+          }
         }
+      });
+      $(window).resize(function () {
+        $('li.menu-item--has-submenu').removeClass('js-opened');
       });
     }
   };

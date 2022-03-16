9726b6ca28ed8478b562dd18077618df129a527d
Kristine <kfernandez@mightycitizen.com>
2022-03-16 13:40:44 -0500

    countdown

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -16,6 +16,63 @@ import './plugins/vimeo';
 import './plugins/sticky-anchor';
 import './plugins/slick';
 
+class Countdown {
+  constructor(selector){
+    this.options = {
+      dateAttr: 'data-countdown',
+      daysSelector: '[data-countdown-property="days"]',
+      hoursSelector: '[data-countdown-property="hours"]',
+      minutesSelector: '[data-countdown-property="min"]',
+      secondsSelector: '[data-countdown-property="sec"]',
+    }
+
+    this.$holder = $(selector);
+    this.date = this.$holder.attr(this.options.dateAttr) * 1000;
+  }
+  timer(){
+    const self = this;
+    var countDownDate = new Date(this.date).getTime();
+    //console.log(countDownDate);
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get todays date and time
+        var now = new Date().getTime();
+        //console.log(now);
+
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+        //console.log(distance);
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        // console.log(hours);
+        // console.log(minutes);
+        const formatString = (string) => {
+          return string.toString().padStart(2, '0');
+        }
+        if (days >= 0) self.$holder.find(self.options.daysSelector).text(formatString(days));
+        if (hours >= 0) self.$holder.find(self.options.hoursSelector).text(formatString(hours));
+        if (minutes >= 0) self.$holder.find(self.options.minutesSelector).text(formatString(minutes));
+        if (seconds >= 0) self.$holder.find(self.options.secondsSelector).text(formatString(seconds));
+
+        // If the count down is over, write some text
+        if (distance <= 0) {
+            clearInterval(x);
+            //window.location.reload();
+        }
+    }, 1000);
+    }
+
+  init(){
+    this.timer()
+  }
+}
+
 //import { mediumBreakpoint, largeBreakpoint, xxlargeBreakpoint } from '../../_patterns/global/base/breakpoints.json'; // Foundation breakpoints
 
 window.jQuery = $;
@@ -109,6 +166,15 @@ const initSmoothScroll = () => {
     }
   });
 }
+
+
+// @countdown init
+const initCountdown = () => {
+  const countdownInstance = new Countdown('[data-countdown]');
+  countdownInstance.init();
+
+}
+
 
 // @table-scroll init
 const initTableScroll = () => {
@@ -234,7 +300,7 @@ $(document).ready(function(){
   initSkipTo(); // @skip-to init
   initSelectUrl(); // @select-url init
 
-
+  initCountdown();
   initFoundationHelpers(); // @foundation helpers init
 })
 

19c601111aa8e9e12dcbc0a62d74696c13ee63b3
Kristine <kfernandez@mightycitizen.com>
2023-01-19 13:59:19 -0600

    alert story

---
+++ src/_patterns/components/informational/alert.stories.jsx
@@ -0,0 +1,37 @@
+import twig from './alert.twig';
+
+//More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
+export default {
+  title: 'Informational/Alert',
+  // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
+  argTypes: {
+
+    label: {
+      control: 'text'
+    },
+    link: {
+      control: 'text'
+    },
+    closable: {
+      control: 'boolean'
+    }
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/html/writing-stories/introduction#using-args
+const Template = ({ label, ...args }) => {
+  // You can either use a function to create DOM elements or use a plain html string!
+  // return `<div>${label}</div>`;
+  return twig({ label, ...args });
+};
+
+const defaultArgs = {
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
+  label: "Alert",
+  link:'#'
+}
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/html/writing-stories/args
+Default.args = defaultArgs;
+
+

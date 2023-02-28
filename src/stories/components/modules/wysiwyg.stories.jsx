4720830eb7c1e7325d909f3149a602205c09f717
Kristine <kfernandez@mightycitizen.com>
2023-02-28 14:27:30 -0600

    add module components

---
+++ src/stories/components/modules/wysiwyg.stories.jsx
@@ -0,0 +1,43 @@
+import twig from './wysiwyg.twig';
+
+//More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Modules/WYSIWYG',
+  // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
+  argTypes: {
+
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/html/writing-stories/introduction#using-args
+const Template = ({ label, ...args }) => {
+  // You can either use a function to create DOM elements or use a plain html string!
+  // return `<div>${label}</div>`;
+  return twig({ label, ...args });
+};
+
+
+const cards = [...Array(5)].map((_, i) => ( {
+  heading: {
+    text: 'Heading',
+    url: '#',
+    level: 3
+  },
+  image: {
+    src: 'https://source.unsplash.com/9wg5jCEPBsw/430x230',
+    alt: 'Alt Text'
+  },
+  link: {
+    text: 'Link',
+    url: '#'
+  },
+}));
+
+const defaultArgs = {
+  heading: 'Heading',
+  description: 'Description',
+  cards: cards
+}
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/html/writing-stories/args
+Default.args = defaultArgs;

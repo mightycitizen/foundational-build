11d4194cd44a299bc7ac0cab5c6850a9606c1940
Kristine Fernandez <kfernandez@kristinandezmac.lan>
2023-03-17 15:11:02 -0500

    homepage

--- src/stories/pages/home.stories.jsx
+++ src/stories/pages/home.stories.jsx
@@ -1,5 +1,6 @@
 import twig from './home.twig';
 
+import { menu, social, logo } from '../global/placeholders/global.json';
 //More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
 export default {
   title: 'Pages/Home',
@@ -27,7 +28,10 @@ const Home = ({ label, ...args }) => {
 
 const defaultArgs = {
   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
-  label: "Alert"
+  label: "Alert",
+  menu,
+  social,
+  logo
 }
 // export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/html/writing-stories/args

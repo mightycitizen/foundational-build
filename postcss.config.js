3d43c20fcc05f18bf962499c44ed4b461813a356
Kristine Fernandez <kfernandez@Kristine-Fernandez-MacBook-Pro.local>
2024-09-26 15:46:01 -0500

    npm start

--- postcss.config.js
+++ postcss.config.js
@@ -2,16 +2,6 @@ export default {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
-      'postcss-pxtorem': {
-        rootValue: 16,
-        unitPrecision: 5,
-        propList: ['*'],
-        selectorBlackList: [],
-        replace: true,
-        mediaQuery: true,
-        minPixelValue: 1,
-        exclude: /node_modules/i,
-      },
       ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
     },
   }
